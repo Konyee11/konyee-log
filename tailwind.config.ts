ccbe424420b84e79adfaf893dc01e2aea08bdaa8
Konyee11 <konyee.527@gmail.com>
2025-01-19 21:51:11 +0900

    tailwind.config.tsにtypographyプラグインを追加し、フォントファミリーを設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            fontFamily: {
+                sans: ["Noto Sans JP", "sans-serif"],
+                mono: ["Source Code Pro", "monospace"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [typography],
 } satisfies Config;
